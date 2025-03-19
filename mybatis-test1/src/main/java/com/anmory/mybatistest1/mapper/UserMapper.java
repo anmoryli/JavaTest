614554d18465a2b94d304380807ad200dcdfa25e
anmoryli <1290189306@qq.com>
2025-03-19 14:54:16 +0800

    mybatis_test

--- mybatis-test1/src/main/java/com/anmory/mybatistest1/mapper/UserMapper.java
+++ mybatis-test1/src/main/java/com/anmory/mybatistest1/mapper/UserMapper.java
@@ -1,8 +1,7 @@
 package com.anmory.mybatistest1.mapper;
 
 import com.anmory.mybatistest1.model.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -16,4 +15,27 @@ import java.util.List;
 public interface UserMapper {
     @Select("select * from user_info")
     List<User> selectAll();
+
+    @Select("select * from user_info where username = #{name}")
+    List<User> selectByName(String name);
+
+    @Insert("insert into user_info (username, password) values (#{username},#{password})")
+    Integer insertUser(String username,String password);
+
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    @Insert("insert into user_info (username, password) VALUES (#{user.username}, #{user.password})")
+    Integer insertUser2(@Param("user") User user);
+
+    @Delete("delete from user_info where id = #{user.id}")
+    void deleteUser(@Param("user") User user);
+
+    @Update("update user_info set username = #{user.username},password = #{user.password} where id = #{user.id}")
+    void updateUser(@Param("user") User user);
+
+    List<User> selectAllXml();
+
+    Integer insertXml(@Param("user") User user);
+
+    void deleteXml(@Param("user") User user);
+
 }
--- mybatis-test1/src/test/java/com/anmory/mybatistest1/mapper/UserMapperTest.java
+++ mybatis-test1/src/test/java/com/anmory/mybatistest1/mapper/UserMapperTest.java
@@ -19,4 +19,65 @@ class UserMapperTest {
         List<User> list = userMapper.selectAll();
         System.out.println(list);
     }
+
+    @Test
+    void selectByName() {
+        System.out.println(userMapper.selectByName("anmory"));
+    }
+
+    @Test
+    void insertUser() {
+        int i = userMapper.insertUser("test","test");
+        System.out.println("影响行数:" + i);
+    }
+
+    @Test
+    void insertUser2() {
+        User user = new User();
+        user.setUsername("getId");
+        user.setPassword("getId");
+        int i = userMapper.insertUser2(user);
+        int id = user.getId();
+        System.out.println("影响了" + i + "行," + "id为:" + id);
+    }
+
+
+    @Test
+    void deleteUser() {
+        User user1 = new User();
+        user1.setId(4);
+        userMapper.deleteUser(user1);
+    }
+
+    @Test
+    void updateUser() {
+        User user2 = new User();
+        user2.setId(1);
+        user2.setUsername("huanhuan");
+        user2.setPassword("lovehuanhuan");
+        userMapper.updateUser(user2);
+    }
+
+    @Test
+    void selectAllXml() {
+        System.out.println(userMapper.selectAllXml());
+    }
+
+    @Test
+    void insertXml() {
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("test");
+        Integer i = userMapper.insertXml(user);
+        System.out.println(i);
+        int id = user.getId();
+        System.out.println(id);
+    }
+
+    @Test
+    void deleteXml() {
+        User user = new User();
+        user.setId(5);
+        userMapper.deleteXml(user);
+    }
 }
