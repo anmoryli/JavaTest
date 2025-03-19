65033c4215fdd7bf32a864272370eebb376e73f6
anmoryli <1290189306@qq.com>
2025-03-19 15:05:15 +0800

    logging

---
+++ logger/src/main/java/com/anmory/logger/Logger.java
@@ -0,0 +1,16 @@
+package com.anmory.logger;
+
+import lombok.extern.slf4j.Slf4j;
+/**
+ * @author Anmory/李梦杰
+ * @description TODO
+ * @date 2025-03-19 下午3:00
+ */
+
+@Slf4j
+public class Logger {
+    public static void main(String[] args) {
+        log.info("这是一个日志");
+    }
+
+}
