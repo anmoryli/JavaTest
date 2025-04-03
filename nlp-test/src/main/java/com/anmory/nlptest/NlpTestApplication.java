80522d96aae382af8160188bc37fbea0660e49c4
anmoryli <1290189306@qq.com>
2025-04-03 21:19:29 +0800

    nlp_test

---
+++ nlp-test/src/main/java/com/anmory/nlptest/NlpTestApplication.java
@@ -0,0 +1,13 @@
+package com.anmory.nlptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlpTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NlpTestApplication.class, args);
+    }
+
+}
---
+++ nlp-test/src/test/java/com/anmory/nlptest/NlpTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anmory.nlptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlpTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
